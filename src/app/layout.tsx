752dfd44daf8f58303a97cbb4a83ca8cbf5c2321
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-09-15 09:59:52 -0700

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+// Run app: `pnpm run dev`
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
