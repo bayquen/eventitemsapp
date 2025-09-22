f3e34585d239644113c6384f00afa9bb83c223cc
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-09-22 14:22:59 -0700

    Change event form details

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -89,12 +89,12 @@ export default function CreateEventPage() {
 
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
-              Date & Time
+              Date
             </label>
             <input
               id="date"
               name="date"
-              type="datetime-local"
+              type="date"
               value={formData.date}
               onChange={handleChange}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
