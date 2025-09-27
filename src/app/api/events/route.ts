ce4e81f0ab3c1674eea97e8e158a0314d49dd147
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-09-26 21:23:15 -0700

    Edit URL slug generation

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -15,12 +15,13 @@ const secondWord = secondWords[Math.floor(Math.random() * secondWords.length)];
 function generateSlug(): string {
   // Return concatenated URL slug for event's main link
   const unique = nanoid(15);
-  return `${firstWord}-${secondWord}-${unique}`  // e.g. "happy-chimp-a3X9k25vRk9L"
+  return `${firstWord}-${secondWord}-${unique}`  // e.g. "happy-chimp-a3X9h..."
 }
 
-  
+// Brandon's TODO: (AFTER MVP in production?) - For a given event, match first two generated words for
+//      main URL slug name match w/ HOST URL slug (e.g. "event/vibey-cat..." for both URLs). Reason: for consistency.
 function generateEditCode(): string {
-  const secureUnique = nanoid(25)    // Longer slug = more secure for host event-editing access
+  const secureUnique = nanoid(20)    // Longer slug = more secure for host event-editing access
   // Return concatenated URL slug for event-host editing link
   return `${firstWord}-${secondWord}-${secureUnique}`
 }
